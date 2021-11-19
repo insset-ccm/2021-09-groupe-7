3155a212f71447ecd36e1e63a7db116e0121322a
CAPOU <capou.mj@gmail.com>
2021-11-19 16:48:50 +0100

    debugger: database

---
+++ Application_Mobile/app/src/main/java/com/capou/application/firebase/Firebase.kt
@@ -0,0 +1,9 @@
+package com.capou.application.firebase
+
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+class Reference {
+
+    private val database = Firebase.database
+}
