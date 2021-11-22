c06c6a3f805dcf845ed1b8ab3a5bf2596fc96a3e
CAPOU <capou.mj@gmail.com>
2021-11-22 19:23:52 +0100

    debugger: database suite

--- Application_Mobile/app/src/main/java/com/capou/application/ui/details/DetailsFragment.kt
+++ Application_Mobile/app/src/main/java/com/capou/application/ui/details/DetailsFragment.kt
@@ -10,19 +10,21 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
-import com.capou.application.api.Api
+import com.capou.application.api.DetailsProduct
 import com.capou.application.api.DetailsRoom
 import com.capou.application.databinding.FragmentDetailsBinding
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Call
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
 
 
 class DetailsFragment : Fragment {
 
     val customerName: String
+
 
     constructor(customerName: String) : super() {
         this.customerName = customerName
@@ -34,7 +36,7 @@ class DetailsFragment : Fragment {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
-
+    val data = Firebase.database.reference
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -47,12 +49,12 @@ class DetailsFragment : Fragment {
         _binding = FragmentDetailsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        context?.let {
+        /*context?.let {
             Glide.with(it)
                 .load("https://cdn.pixabay.com/photo/2016/03/26/16/44/tomatoes-1280859_960_720.jpg")
                 .into(binding.imageView)
-        }
-        binding.titleProduct.text = this.customerName;
+        }*/
+      //  binding.titleProduct.text = this.customerName;
 
         val detail: TextView = binding.detailProduct
         detailsViewModel.detail.observe(viewLifecycleOwner,{
@@ -67,6 +69,61 @@ class DetailsFragment : Fragment {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        //val data = Firebase.database.reference
+        Log.d("Bonjour",data.toString())
+        //val issuccess = data.setValue("Bonjour").isSuccessful
+        // Log.d("Bon",issuccess.toString())
+      /*  data.setValue("H -")
+            .addOnSuccessListener {
+                Log.d("Bonj","yes")
+
+            }
+            .addOnFailureListener { ex : Exception ->
+                Log.d("TAG", ex.toString())
+
+            }
+    */
+        data.addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                Log.d("Bon",snapshot.toString())
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+                Log.d("Bon", error.message)
+            }
+
+        })
+
+        data.child("aliments").addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+               val list =  snapshot.getValue(DetailsProduct::class.java)
+                Log.d("Bonj",list.toString())
+            //  binding.titleProduct.text = list?.nom.toString()
+               /* Glide.with(context!!)
+                    .load(list?.images.toString())
+                    .into(binding.imageView)*/
+             /*  snapshot.children.map {
+
+                       Glide.with(context!!)
+                           .load(it.value.toString())
+                           .into(binding.imageView)
+
+               }*/
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+                Log.d("Bon", error.message)
+            }
+
+        })
     }
 
 }
