a1a74a03a40ee8c98cd139dfacf31a9aab05ac1a
unknown <abdoulayesadjobah1@gmail.com>
2022-01-21 09:45:57 +0100

    ajout profile maraicher

--- Application_Mobile/app/src/main/java/com/capou/application/ui/maraicher/listAlimentMaraicher/MaraicherListAlimentFragment.kt
+++ Application_Mobile/app/src/main/java/com/capou/application/ui/maraicher/listAlimentMaraicher/MaraicherListAlimentFragment.kt
@@ -1,15 +1,21 @@
 package com.capou.application.ui.maraicher.listAlimentMaraicher
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.capou.application.R
 import com.capou.application.databinding.FragmentListAlimentMaraicherBinding
+import com.capou.application.model.AlimentModel
+import com.capou.application.ui.details.DetailsFragment
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
@@ -45,8 +51,12 @@ class MaraicherListAlimentFragment : Fragment() {
             binding.nom.text.clear()
         }
 
+
+
         return root
+
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
