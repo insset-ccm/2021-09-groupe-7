0779856f89b00f6e3ae21ca86836f255ce8cd936
unknown <abdoulayesadjobah1@gmail.com>
2022-04-18 11:43:11 +0200

    add google map

--- Application_Mobile/app/src/main/java/com/capou/application/ui/home/HomeFragment.kt
+++ Application_Mobile/app/src/main/java/com/capou/application/ui/home/HomeFragment.kt
@@ -12,15 +12,26 @@ import androidx.lifecycle.ViewModelProvider
 import com.capou.application.R
 import com.capou.application.databinding.FragmentHomeBinding
 import com.capou.application.ui.details.DetailsFragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import java.util.*
+
 
 class HomeFragment : Fragment() {
 
+    private lateinit var mMap: GoogleMap
+    private var data = Firebase.database.reference
+    private lateinit var binding: FragmentHomeBinding
     private lateinit var homeViewModel: HomeViewModel
-    private var _binding: FragmentHomeBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    // private var _binding: FragmentHomeBinding? = null
+    //private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,40 +41,48 @@ class HomeFragment : Fragment() {
         homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-       /* val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })*/
-
-      /*  supportFragmentManager.commit {
-            replace<ExampleFragment>(R.id.fragment_container)
-            setReorderingAllowed(true)
-            addToBackStack(null)
-        }*/
-
-      /*  binding.starDetails.setOnClickListener {
-           val fragment:Fragment = DetailsFragment("tomate","defautl");
-
-
-            //getFragmentManager
-
-            /* supportFragmentManager.commit {
-                replace<ExampleFragment>(R.id.fragment_container)
-                setReorderingAllowed(true)
-                addToBackStack(null)
-            }*/
-            val fragmentManager : FragmentManager = parentFragmentManager
-            fragmentManager.beginTransaction().replace(R.id.nav_host_fragment_activity_main, fragment).commit()
-
-        }*/
         return root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onStart() {
+        super.onStart()
+        val mapFragment = childFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync( object : OnMapReadyCallback {
+            override fun onMapReady(googleMap: GoogleMap) {
+           mMap = googleMap
+
+            val currentCity = LatLng(49.83689727421656, 3.3002214823430656)
+            val zoom = 55f
+            mMap.addMarker(MarkerOptions().position(currentCity).title("Insset saint quentin"))
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(currentCity))
+
+            setMapLongClick(mMap)
+            setPoiClick(mMap)
+
+        } })
     }
+    private fun setMapLongClick(map: GoogleMap) {
+        map.setOnMapClickListener {
+            val snippet = String.format(
+                Locale.getDefault(),
+                "Lat: %15.5f, Lng: %25.5f",
+                it.latitude,
+                it.longitude
+            )
+            map.addMarker(MarkerOptions().position(it).
+            title("pick up ").snippet(snippet))
+
+        }
+    }
+    private fun setPoiClick(map: GoogleMap) {
+        map.setOnPoiClickListener { point ->
+            val poiMarker = map.addMarker(MarkerOptions().position(point.latLng).title(point.name))
+            poiMarker?.showInfoWindow()
+        }
+    }
+
 }
